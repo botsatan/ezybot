7143981e198bd3a6171c6eb40843c2a3dd9ff5d6
@XYZ <34183912+PASUNX@users.noreply.github.com>
2019-05-09 23:00:29 +0700

    Update ytvideo.py

--- ytvideo.py
+++ ytvideo.py
@@ -2,16 +2,20 @@ import requests, json
 import urllib.request
 from bs4 import BeautifulSoup
 
-def ytSearch(song):
-    if song == None:
-        return "ชื่อวิดีโอไม่สามารถเว้นว่างได้"
-    if song != None:
-        ssong = str(song)
-        query = urllib.parse.quote(ssong)
-        yturl = "https://www.youtube.com/results?search_query=" + query
-        response = urllib.request.urlopen(yturl)
-        ht = response.read()
-        soup = BeautifulSoup(ht, 'html.parser')
-        for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
-            req = requests.get("https://pasunx.tk/api/urlshorten.php?url=" + "https://www.youtube.com/watch?v=" + vid['href'])
-            return(req.json()['text'])
+class youtubeSearch:
+    def __init__(self, query):
+        if not isinstance(query, str):
+            raise ValueError('Query must be strings')
+        if query.replace(' ','') != '':
+            self.urls = {}
+            searchURL = f"https://www.youtube.com/results?search_query={urllib.parse.quote(query)}"
+            response = urllib.request.urlopen(searchURL)
+            soup = BeautifulSoup(response.read(), 'html.parser')
+            for video in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
+                self.urls[len(self.urls)] = {'title': video['title'], 'url': "https://www.youtube.com/watch?v=" + video['href']}
+
+    def __repr__(self):
+        return '%s(%s)' % (self.__class__.__name__, ', '.join(['%s=%r' % (key, value) for key, value in self.__dict__.items()]))
+    
+if __name__ == '__main__':
+    print(youtubeSearch('ยอม legendboy'))
