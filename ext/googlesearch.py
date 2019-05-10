24227c4afee8f9a91644c305ce4e91d09bc0a127
Nack Itthithat <43494421+orzki@users.noreply.github.com>
2019-05-10 11:23:41 +0700

    [⚠️] 🔧 Changed User-Agent.

--- ext/googlesearch.py
+++ ext/googlesearch.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 def googlesearch(search):
-    req = requests.get("https://www.google.com/search", params={"q":search} headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'})
+    req = requests.get("https://www.google.com/search", params={"q":search} headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:66.0) Gecko/20100101 Firefox/66.0'})
     soup = BeautifulSoup(req.text, 'html.parser')
     f_results = []
     r_block = soup.find_all('div', attrs={'class': 'g'})
