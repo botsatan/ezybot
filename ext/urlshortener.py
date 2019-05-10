0704d1d1562399fdb75e301211797383049607e3
Nack Itthithat <43494421+orzki@users.noreply.github.com>
2019-05-10 13:19:55 +0700

    ✨ Added : UrlShortener

---
+++ ext/urlshortener.py
@@ -0,0 +1,9 @@
+import requests
+
+def urlshort(url):
+	api = "https://pasunx.tk/api/urlshorten.php"
+	if url.startswith("http://" or "https://"):
+		short = requests.post(api, params={"url":url})
+		return short.json()['url']
+	else:
+		return 'ERR'
