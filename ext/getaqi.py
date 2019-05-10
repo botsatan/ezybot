6584bbd09bdf50d2fb5398220ca3dca9a6c44c0b
Nack Itthithat <43494421+orzki@users.noreply.github.com>
2019-05-10 11:45:54 +0700

    🔮 Updated : AQI

--- ext/getaqi.py
+++ ext/getaqi.py
@@ -1,5 +1,17 @@
 import requests
 
-def getAQI(stations):
+def getRaw(stations):
 	req = requests.get("http://air4thai.net/forappV2/getAQI_JSON.php")
 	return req.json()['stations'][stations]
+
+def getPM25(stations):
+	req = requests.get("http://air4thai.net/forappV2/getAQI_JSON.php")
+	return req.json()['stations'][stations]['AQILast']['PM25']['value']
+
+def getPM10(stations):
+	req = requests.get("http://air4thai.net/forappV2/getAQI_JSON.php")
+	return req.json()['stations'][stations]['AQILast']['PM10']['value']
+
+def getPM25(stations):
+	req = requests.get("http://air4thai.net/forappV2/getAQI_JSON.php")
+	return req.json()['stations'][stations]['AQILast']['AQI']['aqi']
