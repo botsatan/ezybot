1426ba592b9ebdb7106bd518a9a76c2d320a5fac
@XYZ <34183912+PASUNX@users.noreply.github.com>
2019-05-10 11:24:52 +0700

    Update ytvideo.py

--- ext/ytvideo.py
+++ ext/ytvideo.py
@@ -7,12 +7,12 @@ class youtubeSearch:
         if not isinstance(query, str):
             raise ValueError('Query must be strings')
         if query.replace(' ','') != '':
-            self.urls = {}
+            self.result = []
             searchURL = f"https://www.youtube.com/results?search_query={urllib.parse.quote(query)}"
             response = urllib.request.urlopen(searchURL)
             soup = BeautifulSoup(response.read(), 'html.parser')
             for video in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
-                self.urls[len(self.urls)] = {'title': video['title'], 'url': "https://www.youtube.com/watch?v=" + video['href']}
+                self.result.append({'title': video['title'], 'url': f"https://www.youtube.com/watch?v={video['href']}"})
 
     def __repr__(self):
         return '%s(%s)' % (self.__class__.__name__, ', '.join(['%s=%r' % (key, value) for key, value in self.__dict__.items()]))
