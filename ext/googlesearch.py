c06f756584cf7d6285edb36da9aeecb29dd6631c
@XYZ <34183912+PASUNX@users.noreply.github.com>
2019-05-10 11:26:59 +0700

    Update googlesearch.py

--- ext/googlesearch.py
+++ ext/googlesearch.py
@@ -1,20 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-def googlesearch(search):
-    req = requests.get("https://www.google.com/search", params={"q":search} headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:66.0) Gecko/20100101 Firefox/66.0'})
-    soup = BeautifulSoup(req.text, 'html.parser')
-    f_results = []
-    r_block = soup.find_all('div', attrs={'class': 'g'})
-    for result in r_block:
-        link = result.find('a', href=True)
-        title = result.find('h3')
-        description = result.find('span', attrs={'class': 'st'})
-        if link and title:
-            link = link['href']
-            title = title.get_text()
-            if description:
-                description = description.get_text()
-            if link != '#':
-                f_results.append({'search': search, 'title': title, 'description': description, 'link': link})
-    return f_results
+class googleSearch:
+    def __init__(self, query):
+        if not isinstance(query, str):
+            raise ValueError('Query must be strings')
+        if query.replace(' ','') != '':
+            self.result = []
+            headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:66.0) Gecko/20100101 Firefox/66.0'}
+            req = requests.get("https://www.google.com/search", params={"q": query})
+            soup = BeautifulSoup(req.text, 'html.parser')
+            for result in soup.find_all('div', attrs={'class': 'g'}):
+                link = result.find('a', href=True)
+                title = result.find('h3')
+                description = result.find('span', attrs={'class': 'st'})
+                if link and title:
+                    if link != '#':
+                        self.result.append({'title': title.get_text(), 'description': '' if not description else description.get_text(), 'link': link['href']})
+    def __repr__(self):
+        return '%s(%s)' % (self.__class__.__name__, ', '.join(['%s=%r' % (key, value) for key, value in self.__dict__.items()]))
+    
+if __name__ == '__main__':
+    print(googleSearch('ยอม legendboy'))
