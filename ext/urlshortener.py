eda0ef19e5999a017b2993f535677e15a82f0b3c
Nack Itthithat <43494421+orzki@users.noreply.github.com>
2019-05-12 07:07:29 +0700

    🔧 Fixed : 2bugs
    OR & API

--- ext/urlshortener.py
+++ ext/urlshortener.py
@@ -2,9 +2,16 @@ import requests
 
 def urlshort(url):
 	api = "https://pasunx.tk/api/urlshorten.php"
-	if url.startswith("http://" or "https://"):
+	if url.startswith("http://"):
 		short = requests.post(api, params={"url":url})
-		return short.json()['url']
+		return short.json()['text']
 		#return short.json()
 	else:
-		return 'ERR'
+	    return 'ERR'
+		
+	if url.startswith("https://"):
+		short = requests.post(api, params={"url":url})
+		return short.json()['text']
+		#return short.json()
+	else:
+	    return 'ERR'
